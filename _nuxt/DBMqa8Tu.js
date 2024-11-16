2523ed9e70fd904f86d4d59ec9ceb83b32c8af88
nina <ninaw2306@gmail.com>
2024-11-16 18:08:17 +0800

    deploy

---
+++ _nuxt/DBMqa8Tu.js
@@ -0,0 +1,1 @@
+import{c as e,o as n}from"./-anE7dHd.js";const r={__name:"index",setup(a){return(t,c)=>(n(),e("div",null," Page: admin/index "))}};export{r as default};
---
+++ _nuxt/eZieGhmv.js
@@ -0,0 +1,1 @@
+import{c as e,o as r}from"./-anE7dHd.js";const n={__name:"order",setup(o){return(a,t)=>(r(),e("div",null," Page: admin/order "))}};export{n as default};
